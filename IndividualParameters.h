27f7ca1fe10bbf5f4e2de71c29167a5b7b67174a
zizien <82201802+zizien1019@users.noreply.github.com>
2023-11-29 22:21:44 -0500

    don't pull this version, don't

--- IndividualParameters.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// This struct defines the chance for an individual to get infected as well as,
-// the infection period in epochs
-struct IndividualParameters {
-	float Infectiosity = 0.13;
-	std::uint8_t DiseaseDuration = 7;
-};
